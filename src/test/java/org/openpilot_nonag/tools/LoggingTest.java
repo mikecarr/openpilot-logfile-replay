8fee536e56cedd3e0033154ae401be41d5889901
Mike Carr <mcarr67@gmail.com>
2014-07-21 10:25:37 -0700

    change log level to debug

---
+++ src/test/java/org/openpilot_nonag/tools/LoggingTest.java
@@ -0,0 +1,13 @@
+package org.openpilot_nonag.tools;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoggingTest {
+
+    @Test
+    public void testOpenFile() throws Exception {
+
+    }
+}
